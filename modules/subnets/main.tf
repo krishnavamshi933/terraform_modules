20ed05dce0aebb448716b882accaab40ed56a55a
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2024-01-31 16:42:14 +0530

    Create main.tf

---
+++ modules/subnets/main.tf
@@ -0,0 +1,31 @@
+variable "vpc_id" {}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private"
+  }
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
