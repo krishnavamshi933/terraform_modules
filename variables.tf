5d41794951ab6a2ae8374c874a4e57eb0c5ce238
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2024-01-31 16:47:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "region" {
+  description = "The AWS region to deploy resources into"
+  default     = "us-east-1"  # Set your default region or leave it empty
+}
+
+# Add any other variables you need for your configuration
