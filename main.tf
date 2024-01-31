4a0e0dfab21011c63379c550ba200dfcd13cd360
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2024-01-31 16:40:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "subnets" {
+  source   = "./modules/subnets"
+  vpc_id   = module.vpc.vpc_id
+}
+
+module "security_groups" {
+  source   = "./modules/security_groups"
+  vpc_id   = module.vpc.vpc_id
+}
+
+module "key_pair" {
+  source = "./modules/key_pair"
+}
+
+module "instances" {
+  source              = "./modules/instances"
+  public_subnet_id    = module.subnets.public_subnet_id
+  private_subnet_id   = module.subnets.private_subnet_id
+  security_group_ids  = [module.security_groups.instance_sg_id, module.security_groups.private_instance_sg_id]
+  key_name            = module.key_pair.key_name
+}
+
+module "nat" {
+  source              = "./modules/nat"
+  public_subnet_id    = module.subnets.public_subnet_id
+  private_subnet_id   = module.subnets.private_subnet_id
+}
